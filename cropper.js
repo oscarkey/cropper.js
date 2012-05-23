68bd5a70e676dd5ceb3dfb0d988111b9e64b54c9
Oscar Key <oscar@keyfamily.org.uk>
2012-05-23 16:41:11 +0000

    Fixed missing semicolon in last commit.

--- cropper.js
+++ cropper.js
@@ -263,13 +263,13 @@
 		var byteString = atob(dataURI.split(',')[1]);
 		
 		// separate out the mime component
-		var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+		var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
 		
 		// write the bytes of the string to an ArrayBuffer
 		var ab = new ArrayBuffer(byteString.length);
 		var ia = new Uint8Array(ab);
 		for (var i = 0; i < byteString.length; i++) {
-		    ia[i] = byteString.charCodeAt(i);
+			ia[i] = byteString.charCodeAt(i);
 		}
 		
 		// write the ArrayBuffer to a blob, and you're done
